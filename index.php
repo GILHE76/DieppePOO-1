c4320a5f53d4d1e4b4b05c58d1a2c6967fe2fdb8
Bob Marley <bob@marley.com>
2018-04-27 16:25:03 +0200

    Fichier

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+define("PATHCONF", "./conf/");
 date_default_timezone_set("Europe/Paris");
 require_once "./functions/classAutoLoader.php";
 spl_autoload_register('classAutoLoader');
+
+$test = new Form(PATHCONF, "registration");
+
+$test->frmGenerate();
+
+
